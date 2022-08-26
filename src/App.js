1ae18db0b8a18f6d2e111ab7096e59286c523ada
Stephen Synowsky <ssynowsky@gmail.com>
2022-08-26 17:14:48 -0400

    Displaying the collection & show titles

--- src/App.js
+++ src/App.js
@@ -3,34 +3,31 @@ import { getDisneyData } from './actions';
 import logo from './logo.svg';
 import './App.css';
 
+import Collection from './Containers/Collection';
+
+// runs twice in dev
+// https://beta.reactjs.org/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
+let devUseEffect = false; 
+
 function App() {
   const [state, setState] = useState({});
   useEffect(() => {
-    getDisneyData()
-      .then(res => {
-        setState(res);
-      });
+    if (!devUseEffect) {
+      devUseEffect = true;
+      console.log('useEffect');
+      getDisneyData()
+        .then(res => {
+          console.log('getDisneyData .then')
+          setState(res);
+        });
+    }
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div>
-        {state?.StandardCollection?.containers.map(cont => (
-          <div>{cont.type} | {cont.style}</div>
+        {state?.StandardCollection?.containers.map(coll => (
+          <Collection data={coll} />
         ))}
       </div>
     </div>
